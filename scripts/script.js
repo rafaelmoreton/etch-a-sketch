5db99a5fd21ef9cb12028839314d11eba5157743
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2021-09-12 13:52:22 -0300

    Adicionada funcionalidade alterar o tamanho dos quadrados do grid de acordo com o tamanho do grid para que caibam numa mesma área. Adicionada funcionalidade de alterar os quadrados para uma cor aleatória e degradê.

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,84 @@
 const btnCntainr = document.querySelector(".buttons");
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    let column = document.createElement("div");
+// Initial setting
+let gridSize = 16;
+let adjustedWidth = "20px";
+let adjustedHeight = "20px";
+let colorfull = 0;
+let blacknwhite = 0;
+
+// Universal scope function variables
+let newHeight;
+let newWidth;
+let column;
+let square;
+let gridLoopCounter;
+let currentSqrColor;
+
+function createGrid() {
+    for (gridLoopCounter = 0; gridLoopCounter < gridSize; gridLoopCounter++) {
+    column = document.createElement("div");
     column.classList.add("column");
     container.appendChild(column);
-    for (let n = 0; n < 16; n++) {
-        let square = document.createElement("div");
-            square.classList.add("sqrPixel")
+        for (let n = 0; n < gridSize; n++) {
+            let square = document.createElement("div");
+            square.classList.add("sqrPixel");
+            square.style.width = adjustedWidth;
+            square.style.height = adjustedHeight;
+            square.style.backgroundColor = "rgb(255, 255, 255)"
             square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
-        });
-        column.appendChild(square);
+                if (colorfull === 0 && blacknwhite === 0) {
+                    square.style.backgroundColor = "rgb(0, 0, 0)";
+                }
+                if (colorfull === 1) {
+                    square.style.backgroundColor = generateRandomColor();
+                }
+                if (blacknwhite === 1) {
+                    currentSqrColor = (square.style.backgroundColor);
+                    square.style.backgroundColor = darken();
+                }
+            });
+            column.appendChild(square);
+        };
+    };   
+};
+
+function eraseGrid() {
+    for (; gridLoopCounter > 0; gridLoopCounter--) {
+        container.removeChild(container.firstChild);
     };
+};
+
+function redefineGrid(size) {
+    eraseGrid();
+    gridSize = size;
+    adjustedWidth = (320 / gridSize) + "px";
+    adjustedHeight = (320 / gridSize) + "px";
+    createGrid();
+};
+
+function generateRandomColor() {
+    var randomColor = "#"+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
+function darken() {
+    let firstCut = currentSqrColor.replace("rgb(", "");
+    let secondCut = firstCut.replace(")", "");
+    let colorArray = secondCut.split(", ");
+    colorArray[0] -= 25;
+    if (colorArray[0] < 0) {
+        colorArray[0] = 0;
+    };
+    colorArray[1] -= 25;
+    if (colorArray[1] < 0) {
+        colorArray[1] = 0;
+    };
+    colorArray[2] -= 25;
+    if (colorArray[2] < 0) {
+        colorArray[2] = 0;
+    };
+    let darkerColor = "rgb(" + colorArray.join(", ") + ")"
+    return darkerColor;
 }
